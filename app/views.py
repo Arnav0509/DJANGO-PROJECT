cfde73bf4934356b0ebb006b083591170adb9fa9
Arnav0509 <arnavguptaa0509@gmail.com>
2026-08-25 10:08:39 +0000

    A8313:38 PM237 223738i3:38 PM: {25PM3:38 PMe}

--- app/views.py
+++ app/views.py
@@ -3,5 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def bootstrap(request):
     return render(request, 'bootstrap.html')
+def msd(request):
+    return render(request,'msd.html')
 
 # jhgfdsafghgj
--- project10/urls.py
+++ project10/urls.py
@@ -21,4 +21,5 @@ from app.views import*
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('bootstrap/',bootstrap, name='bootstrap'),
+    path('msd/',msd,name='msd'),
 ]
